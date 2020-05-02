e1ff8659b4f9a73c3c808afc790179d3d4b607a1
Karen Nguyen <knguyen4184@yahoo.com>
2020-05-02 15:15:07 -0600

    Change naming from input to idea

--- src/Form.js
+++ src/Form.js
@@ -2,23 +2,23 @@
 import React, { useState } from 'react'
 
 const Form = ({ addIdea }) => {
-  const [input, setInput] = useState({id: Date.now(), title: '', description: ''})
+  const [idea, setIdea] = useState({id: Date.now(), title: '', description: ''})
 
   const handleChange = event => {
     const {name, value} = event.target
-    setInput({...input, [name]: value})
+    setIdea({...idea, [name]: value})
   }
 
   const submitIdea = (event) => {
     event.preventDefault();
-    addIdea(input)
-    setInput({id: Date.now(), title: '',description: ''});
+    addIdea(idea)
+    setIdea({id: Date.now(), title: '',description: ''});
   }
 
   return (
     <form>
-      <input name='title' onChange={handleChange} value={input.title} placeholder='Title..'/>
-      <input name='description' onChange={handleChange} value={input.description} placeholder='Description..'/>
+      <input name='title' onChange={handleChange} value={idea.title} placeholder='Title..'/>
+      <input name='description' onChange={handleChange} value={idea.description} placeholder='Description..'/>
       <button type='submit' onClick={submitIdea}>Submit Idea</button>
     </form>
   )
